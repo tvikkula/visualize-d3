1ba869a89701f9c3ff4718b7d51d017e007edbb3
Tommi Vikkula <tvikkula@gmail.com>
2015-12-01 18:49:38 +0200

    Added a first "working" iteration of the page.

--- js/plot.js
+++ js/plot.js
@@ -1,24 +1,32 @@
 function setup() {
-    "use strict";
+    // Set margins
     var margin = 75,
 	width = 1400 - margin
 	height = 1600 - margin;
+
+    // Set header
+    d3.select('body')
+	.append('h2')
+	.text('Flight cancellations');
+
+    // Create the svg 
     var svg = d3.select('body')
 	.append('svg')
-	.attr('width', width + margin)
-	.attr('heigh', height + margin)
+	    .attr('width', width + margin)
+	    .attr('heigh', height + margin)
         .append('g')
-	.attr('class', 'chart');
-    return margin, svg
+	    .attr('class', 'chart');
+
+    return margin, svg;
 }
 
 function draw(data) {
     var margin, svg = setup();
-
-    d3.select('svg')
-	.selectAll('circle')
-	.data(data)
-	.enter()
-	.append('circle');
-
+    console.log(margin);
+    var myChart = new dimple.chart(svg, data);
+    myChart.addCategoryAxis("x", "Origin");
+    myChart.addMeasureAxis("y", "cancelledTotal");
+    myChart.addSeries(null, dimple.plot.scatter);
+    //myChart.addLegend(60, 10, 510, 20, "right");
+    myChart.draw();
 }
