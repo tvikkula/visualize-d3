bb06be75606bf8a031413cbb1c1437fc25388a3c
Tommi Vikkula <tvikkula@gmail.com>
2015-12-08 19:51:45 +0200

    Made the selection work and adjusted plot size and x-axis labels.

--- js/plot.js
+++ js/plot.js
@@ -1,5 +1,6 @@
 var parseDate = d3.time.format("%U-%Y").parse;
-function draw(error, data) {
+
+function draw(data) {
     data.forEach(function(d) {
 	    d.value = +d.cancelledTotal;
 	    d.week = parseDate(d.WeekOfYear+'-2008');
@@ -9,9 +10,9 @@ function draw(error, data) {
         .append('h2')
         .text('Flight cancellations');
 
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-	width = 1200 - margin.left - margin.right,
-        height = 1000 - margin.top - margin.bottom;
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+	width = 1100 - margin.left - margin.right,
+        height = 580 - margin.top - margin.bottom;
 
     var svg = d3.select('body')
 	.append('svg')
@@ -20,38 +21,29 @@ function draw(error, data) {
 	.append('g')
 	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    d3.select('#cancellationSelect option').addEventListener('click', function(e) {
-	    
-	});
-	
-    // Dimple code:
     var myChart = new dimple.chart(svg, data);
-    var x = myChart.addTimeAxis('x', 'week');
-    var y = myChart.addMeasureAxis('y', 'value');
+    var x = myChart.addTimeAxis('x', 'week', null, '%U');
+    x.ticks = 52;
+    myChart.addMeasureAxis('y', 'value');
     var series = myChart.addSeries('Origin', dimple.plot.line);
     series.addOrderRule('week');
     myChart.addLegend(60, 10, 510, 20, 'right');
     myChart.draw();
 
-    if (error) throw error;
+    document.getElementById('cancellationSelect')
+        .addEventListener('change', function(e) {
+		console.log(e.currentTarget.value);
+                updateData(e.currentTarget.value, myChart);
+            });
 }
 
-function updateData(cancellationType) {
-    d3.csv('data/top_flights_cancellations_byweek.csv', function(error, data) {
+function updateData(cancellationType, myChart) {
+    d3.csv('data/top_flights_cancellations_byweek.csv', function(data) {
 	    data.forEach(function(d) {
 		    d.value = +d[cancellationType];
-		    d.week = +d.WeekByYear;
+		    d.week = parseDate(d.WeekOfYear+'-2008');
 		});
-	    var myChart = new dimple.chart(d3.select('svg'), data);
-	    var x = myChart.addTimeAxis('x', 'week');
-	    var y = myChart.addMeasureAxis('y', 'value');
-	    var series = myChart.addSeries('Origin', dimple.plot.line);
-	    series.addOrderRule('week');
-	    myChart.addLegend(60, 10, 510, 20, 'right');
-	    myChart.draw();
-
-	    if (error) throw error;
+	    myChart.data = data;
+	    myChart.draw(1000);
 	});
-
-    
 }
