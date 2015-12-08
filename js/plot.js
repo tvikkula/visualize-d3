a963e406a7b11fd6058dfdc928e77ac6a4325d98
Tommi Vikkula <tvikkula@gmail.com>
2015-12-08 20:08:25 +0200

    Added a really cool interactive legend to filter Origins.

--- js/plot.js
+++ js/plot.js
@@ -1,7 +1,9 @@
 var parseDate = d3.time.format("%U-%Y").parse;
+var dataCache = {};
 
 function draw(data) {
-    data.forEach(function(d) {
+    dataCache = data;
+    dataCache.forEach(function(d) {
 	    d.value = +d.cancelledTotal;
 	    d.week = parseDate(d.WeekOfYear+'-2008');
 	});
@@ -21,22 +23,67 @@ function draw(data) {
 	.append('g')
 	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    var myChart = new dimple.chart(svg, data);
+    var myChart = new dimple.chart(svg, dataCache);
     var x = myChart.addTimeAxis('x', 'week', null, '%U');
     x.ticks = 52;
     myChart.addMeasureAxis('y', 'value');
     var series = myChart.addSeries('Origin', dimple.plot.line);
     series.addOrderRule('week');
-    myChart.addLegend(60, 10, 510, 20, 'right');
+    var myLegend = myChart.addLegend(60, 10, 510, 20, 'right');
     myChart.draw();
+
+    myChart.legends = [];
+
+    svg.selectAll("title_text")
+	.data(["Click legend to","show/hide owners:"])
+	.enter()
+	.append("text")
+	.attr("x", 499)
+	.attr("y", function (d, i) { return 90 + i * 14; })
+	.text(function (d) { return d; });
+
+
+    var filterValues = dimple.getUniqueValues(dataCache, "Origin");
+    myLegend.shapes.selectAll("rect")
+	.on("click", function (e) {
+		updateLegend(myChart, filterValues, e);
+	    });
 
     document.getElementById('cancellationSelect')
         .addEventListener('change', function(e) {
-		console.log(e.currentTarget.value);
+                console.log(e.currentTarget.value);
                 updateData(e.currentTarget.value, myChart);
             });
 }
 
+// Interactive legend from http://dimplejs.org/advanced_examples_viewer.html?id=advanced_interactive_legends
+function updateLegend(myChart, filterValues, e) {
+    // This indicates whether the item is already visible or not                                                                                                         
+    var hide = false;
+    var newFilters = [];
+    // If the filters contain the clicked shape hide it                                                                                                                  
+    filterValues.forEach(function (f) {
+	    if (f === e.aggField.slice(-1)[0]) {
+		hide = true;
+	    } else {
+		newFilters.push(f);
+	    }
+	});
+    // Hide the shape or show it                                                                                                                                         
+    if (hide) {
+	d3.select(this).style("opacity", 0.2);
+    } else {
+	newFilters.push(e.aggField.slice(-1)[0]);
+	d3.select(this).style("opacity", 0.8);
+    }
+    // Update the filters                                                                                                                                                
+    filterValues = newFilters;
+    // Filter the data                                                                                                                                                   
+    myChart.data = dimple.filterData(dataCache, "Origin", filterValues);
+    // Passing a duration parameter makes the chart animate. Without                                                                                                     
+    // it there is no transition                                                                                                                                         
+    myChart.draw(800);
+}
 function updateData(cancellationType, myChart) {
     d3.csv('data/top_flights_cancellations_byweek.csv', function(data) {
 	    data.forEach(function(d) {
@@ -45,5 +92,6 @@ function updateData(cancellationType, myChart) {
 		});
 	    myChart.data = data;
 	    myChart.draw(1000);
+	    dataCache = data;
 	});
 }
